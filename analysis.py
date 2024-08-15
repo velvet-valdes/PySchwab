e156219ba333e7e98701b43e6c4e20b938b7d6f9
velvet-valdes <valdestr@gmail.com>
2024-08-15 10:37:29 -0400

    comment update

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ symbols = stonks.csv_to_list('input', 500)
 # Create main data frame from ticker symbols
 main_frame = stonks.fetch_and_aggregate_data(symbols,token)
 
-# Process data types for data frame - z-scores not commented out
+# Process data types for data frame
 stonks.prepare_data_frame(main_frame)
 
 # Summarize the data
